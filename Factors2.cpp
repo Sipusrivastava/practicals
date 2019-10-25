1888a75d41b66e9cbdf281b1f620b0aaed0108d4
Sipusrivastava <53932086+Sipusrivastava@users.noreply.github.com>
2019-10-25 09:47:18 +0530

    Update Factors2.cpp

--- Factors2.cpp
+++ Factors2.cpp
@@ -22,4 +22,19 @@ cout<<i<<"\n";
 }
 }
 }
+t main()
+{
+int i,n;
+cout<<"Program to find the factors of the given number";
+cout<<"\n";
+cout<<"\n";
+cout<<"                                        Enter the number of which you want to find the factors="<<" ";
+while(!(cin>>n) || (n<=0))
+{
+    cin.clear();
+    cin.ignore(100,'\n');
+    cout<<"'INVALID INPUT! ENTER A POSITIVE NUMBER:"<<" ";
+}
 
+for(i=1;i<=n;i++)
+{
